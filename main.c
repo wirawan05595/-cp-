19558073e90cc6771dc6324374dbfe1c6b27df7b
wirawan05595 <61070209@kmitl.ac.th>
2019-04-17 15:54:38 +0700

    Decorate and organize
    Decorate and organize

--- main.c
+++ main.c
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 
 static GtkWidget *result;
+static GtkWidget *output;
 static GtkWidget *number1;
 static GtkWidget *combo_box1;
 static GtkWidget *combo_box2;
@@ -16,8 +18,9 @@ void do_calculate(GtkWidget *button2, gpointer data) {
     char* ck1 = (char*)gtk_entry_get_text(GTK_ENTRY(number1));
     char buffer[100];
     int check = 0;
+
 // *********************************** ดักคนใส่ เศษส่วน ****************
-     for (int j = 0; j < 10;j++)
+    for (int j = 0; j < 10;j++)
     {
         if (ck1[j] == 47)
         {
@@ -27,12 +30,14 @@ void do_calculate(GtkWidget *button2, gpointer data) {
             break;
         }
     }
-if (check == 0){
-      double num1 = atof((char *)gtk_entry_get_text(GTK_ENTRY(number1)));
-      gchar *select_scale1 = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(combo_box1));
-      gchar *select_scale2 = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(combo_box2));
 
-      // *********************************** แปลงช้อนชาเป็น... ****************
+if (check == 0)
+    {
+    double num1 = atof((char *)gtk_entry_get_text(GTK_ENTRY(number1)));
+    gchar *select_scale1 = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(combo_box1));
+    gchar *select_scale2 = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(combo_box2));
+
+// *********************************** แปลงช้อนชาเป็น... ****************
     if (strcmp(select_scale1, scale1[0]) == 0 && strcmp(select_scale2, scale2[0]) == 0)
     {
         if (fmod(num1,1) == 0)
@@ -45,6 +50,7 @@ if (check == 0){
             gtk_label_set_text(GTK_LABEL(output), buffer);
         }
     }
+
 
     else if (strcmp(select_scale1, scale1[0]) == 0 && strcmp(select_scale2, scale2[1]) == 0)
     {
@@ -60,6 +66,7 @@ if (check == 0){
         }
     }
 
+
      else if (strcmp(select_scale1, scale1[0]) == 0 && strcmp(select_scale2, scale2[2]) == 0)
     {
         double box = num1/5;
@@ -73,6 +80,7 @@ if (check == 0){
             gtk_label_set_text(GTK_LABEL(output), buffer);
         }
     }
+
 
     else if (strcmp(select_scale1, scale1[0]) == 0 && strcmp(select_scale2, scale2[3]) == 0)
     {
@@ -88,6 +96,7 @@ if (check == 0){
         }
     }
 
+
     else if (strcmp(select_scale1, scale1[0]) == 0 && strcmp(select_scale2, scale2[4]) == 0)
     {
         double box = num1/0.01041666666;
@@ -102,6 +111,7 @@ if (check == 0){
         }
     }
 
+
     else if (strcmp(select_scale1, scale1[0]) == 0 && strcmp(select_scale2, scale2[5]) == 0)
     {
         double box = num1/0.01041666666;
@@ -115,6 +125,7 @@ if (check == 0){
             gtk_label_set_text(GTK_LABEL(output), buffer);
         }
     }
+
 
     else if (strcmp(select_scale1, scale1[0]) == 0 && strcmp(select_scale2, scale2[6]) == 0)
     {
@@ -158,6 +169,7 @@ if (check == 0){
         }
     }
 
+
 // *********************************** แปลงช้อนโต๊ะเป็น... ****************
 
     else if (strcmp(select_scale1, scale1[1]) == 0 && strcmp(select_scale2, scale2[0]) == 0)
@@ -174,6 +186,7 @@ if (check == 0){
         }
     }
 
+
     else if (strcmp(select_scale1, scale1[1]) == 0 && strcmp(select_scale2, scale2[1]) == 0)
     {
         if (fmod(num1,1) == 0)
@@ -186,6 +199,8 @@ if (check == 0){
             gtk_label_set_text(GTK_LABEL(output), buffer);
         }
     }
+
+
 
     else if (strcmp(select_scale1, scale1[1]) == 0 && strcmp(select_scale2, scale2[2]) == 0)
     {
@@ -228,6 +243,7 @@ if (check == 0){
             gtk_label_set_text(GTK_LABEL(output), buffer);
         }
     }
+
 
     else if (strcmp(select_scale1, scale1[1]) == 0 && strcmp(select_scale2, scale2[5]) == 0)
     {
@@ -301,6 +317,8 @@ if (check == 0){
         }
     }
 
+
+
     else if (strcmp(select_scale1, scale1[2]) == 0 && strcmp(select_scale2, scale2[1]) == 0)
     {
         double box = num1*15;
@@ -314,6 +332,7 @@ if (check == 0){
             gtk_label_set_text(GTK_LABEL(output), buffer);
         }
     }
+
 
     else if (strcmp(select_scale1, scale1[2]) == 0 && strcmp(select_scale2, scale2[2]) == 0)
     {
@@ -355,6 +374,7 @@ if (check == 0){
             gtk_label_set_text(GTK_LABEL(output), buffer);
         }
     }
+
 
        else if (strcmp(select_scale1, scale1[2]) == 0 && strcmp(select_scale2, scale2[5]) == 0)
     {
@@ -413,6 +433,7 @@ if (check == 0){
     }
 
 // *********************************** แปลงถ้วยตวงเป็น... ****************
+
     else if (strcmp(select_scale1, scale1[3]) == 0 && strcmp(select_scale2, scale2[0]) == 0)
     {
         double box = num1*0.02083333333;
@@ -455,7 +476,7 @@ if (check == 0){
         }
     }
 
-      else if (strcmp(select_scale1, scale1[3]) == 0 && strcmp(select_scale2, scale2[3]) == 0)
+            else if (strcmp(select_scale1, scale1[3]) == 0 && strcmp(select_scale2, scale2[3]) == 0)
     {
         if (fmod(num1,1) == 0)
         {
@@ -496,7 +517,7 @@ if (check == 0){
         }
     }
 
-        else if (strcmp(select_scale1, scale1[3]) == 0 && strcmp(select_scale2, scale2[6]) == 0)
+            else if (strcmp(select_scale1, scale1[3]) == 0 && strcmp(select_scale2, scale2[6]) == 0)
     {
         double box = num1*4;
         if (fmod(box,1) == 0)
@@ -537,7 +558,6 @@ if (check == 0){
             gtk_label_set_text(GTK_LABEL(output), buffer);
         }
     }
-
 // *********************************** แปลงปอนด์เป็น... ****************
         else if (strcmp(select_scale1, scale1[4]) == 0 && strcmp(select_scale2, scale2[0]) == 0)
     {
@@ -608,7 +628,7 @@ if (check == 0){
         }
     }
 
-      else if (strcmp(select_scale1, scale1[4]) == 0 && strcmp(select_scale2, scale2[5]) == 0)
+        else if (strcmp(select_scale1, scale1[4]) == 0 && strcmp(select_scale2, scale2[5]) == 0)
     {
         if (fmod(num1,1) == 0)
         {
@@ -635,7 +655,7 @@ if (check == 0){
         }
     }
 
-      else if (strcmp(select_scale1, scale1[4]) == 0 && strcmp(select_scale2, scale2[7]) == 0)
+        else if (strcmp(select_scale1, scale1[4]) == 0 && strcmp(select_scale2, scale2[7]) == 0)
     {
         double box = num1*8;
         if (fmod(box,1) == 0)
@@ -649,7 +669,7 @@ if (check == 0){
         }
     }
 
-      else if (strcmp(select_scale1, scale1[4]) == 0 && strcmp(select_scale2, scale2[8]) == 0)
+        else if (strcmp(select_scale1, scale1[4]) == 0 && strcmp(select_scale2, scale2[8]) == 0)
     {
         double box = num1/16;
         if (fmod(box,1) == 0)
@@ -663,8 +683,8 @@ if (check == 0){
         }
     }
 
-    // *********************************** แปลงไฟน์เป็น... ***************
-      else if (strcmp(select_scale1, scale1[5]) == 0 && strcmp(select_scale2, scale2[0]) == 0)
+    // *********************************** แปลงไฟน์เป็น... ****************
+        else if (strcmp(select_scale1, scale1[5]) == 0 && strcmp(select_scale2, scale2[0]) == 0)
     {
         double box = num1/96;
         if (fmod(box,1) == 0)
@@ -788,7 +808,7 @@ if (check == 0){
         }
     }
 
-    // *********************************** แปลงควอทเป็น... ***************
+    // *********************************** แปลงควอทเป็น... ****************
 
         else if (strcmp(select_scale1, scale1[6]) == 0 && strcmp(select_scale2, scale2[0]) == 0)
     {
@@ -914,7 +934,6 @@ if (check == 0){
             gtk_label_set_text(GTK_LABEL(output), buffer);
         }
     }
-
     // *********************************** แปลงแกลลอนเป็น... ****************
         else if (strcmp(select_scale1, scale1[7]) == 0 && strcmp(select_scale2, scale2[0]) == 0)
     {
@@ -1041,7 +1060,7 @@ if (check == 0){
         }
     }
 
-    //*********************************** แปลงออนซ์เป็น... ****************
+    // *********************************** แปลงออนซ์เป็น... ****************
         else if (strcmp(select_scale1, scale1[8]) == 0 && strcmp(select_scale2, scale2[0]) == 0)
     {
         double box = num1/6;
@@ -1168,23 +1187,25 @@ if (check == 0){
     }
 }
 }
-static void libraryscale(GtkApplication *app,GtkWidget *widget, gpointer data) {
-	GtkWidget *window;
-	GtkWidget *grid;
+
+
+
+
+
+
+
+
+static void help(GtkApplication *app,GtkWidget *widget, gpointer data) {
+    GtkWidget *window;
+    // GtkWidget *grid;
     window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title (GTK_WINDOW (window), "Second Window");
     gtk_container_set_border_width (GTK_CONTAINER (window), 50);
     gtk_widget_set_size_request (window, 500, 500);
-    grid = gtk_grid_new ();
-    gtk_container_add (GTK_CONTAINER (window), grid);
+    // grid = gtk_grid_new ();
+    // gtk_container_add (GTK_CONTAINER (window), grid);
     gtk_window_set_position (GTK_WINDOW (window), GTK_WIN_POS_CENTER);
 
-    result = gtk_label_new("1: กรัม");
-    gtk_grid_attach(GTK_GRID(grid), result, 0, -1, 1, 1);
-    result = gtk_label_new("2: ช้อนชา");
-    gtk_grid_attach(GTK_GRID(grid), result, 0, 0, 1, 1);
-    result = gtk_label_new("3: ช้อนโต๊ะ");
-    gtk_grid_attach(GTK_GRID(grid), result, 0, 1, 1, 1);
 
 
 
@@ -1199,45 +1220,73 @@ static void libraryscale(GtkApplication *app,GtkWidget *widget, gpointer data) {
 
 
 static void section(GtkApplication *app,GtkWidget *widget, gpointer data) {
-	GtkWidget *window,*grid,*box;
-	GtkWidget *button;
-	window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title (GTK_WINDOW (window), "Second Window");
+    GtkWidget *window,*grid,*box;
+    GtkWidget *button,*button2;
+    GtkWidget *image2;
+    GtkWidget *layout;
+
+
+    window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title (GTK_WINDOW (window), "cp61");
+    gtk_container_set_border_width (GTK_CONTAINER (window), 0);
+    gtk_window_set_default_size(GTK_WINDOW(window), 1000, 800);
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+    layout = gtk_layout_new(NULL, NULL);
+    gtk_container_add(GTK_CONTAINER (window), layout);
+    gtk_widget_show(layout);
+    image2 = gtk_image_new_from_file("2.png");
+    gtk_layout_put(GTK_LAYOUT(layout), image2, 0, 0);
     grid = gtk_grid_new ();
     gtk_container_add (GTK_CONTAINER (window), grid);
 
 
-	button = gtk_button_new_with_label ("มาตราส่วน");
-  	g_signal_connect (button, "clicked", G_CALLBACK (libraryscale), (gpointer) window);
-  	gtk_grid_attach(GTK_GRID(grid), button, 0, 0, 1, 1);
-  	gtk_container_set_border_width (GTK_CONTAINER (window), 250);
 
 
 
-    result = gtk_label_new("มาตรส่วนที่มี:");
+    button = gtk_button_new();
+     image2 = gtk_image_new_from_file("help.png");
+    gtk_button_set_image(GTK_BUTTON(button), image2);
+    gtk_button_set_relief(GTK_BUTTON(button), GTK_RELIEF_NONE);
+    g_signal_connect (button, "clicked", G_CALLBACK (help), (gpointer) window);
+    gtk_layout_put(GTK_LAYOUT(layout), button, 50, 0);
+    gtk_grid_attach(GTK_GRID(grid), button, 0, 0, 1, 1);
+
+
+
+    result = gtk_label_new(NULL);
+    gtk_label_set_markup (GTK_LABEL (result), "<span face=\"Courier New\" font=\"20\" color=\"#006633\"bgcolor=\"#00CC66\"><b>The scale has:</b></span>");
+    gtk_layout_put(GTK_LAYOUT(layout), result, 110, 250);
     gtk_grid_attach(GTK_GRID(grid), result, 2, 1, 1, 1);
 
 
-
-  combo_box1 = gtk_combo_box_text_new();
+     combo_box1 = gtk_combo_box_text_new();
      gtk_layout_put(GTK_LAYOUT(layout), combo_box1, 450, 250);
     gtk_grid_attach(GTK_GRID(grid), combo_box1, 3, 1, 1, 1);
 
+    for (int i = 0; i < G_N_ELEMENTS(scale1); i++){
+        gtk_combo_box_text_append_text (GTK_COMBO_BOX_TEXT(combo_box1), scale1[i]);
+    }
+    gtk_combo_box_set_active(GTK_COMBO_BOX(combo_box1), 0);
 
-    result = gtk_label_new("  ");
-    gtk_grid_attach(GTK_GRID(grid), result, 3, 2, 1, 1);
-
-    result = gtk_label_new("ปริมาณที่ต้องการ:  ");
+    result = gtk_label_new(NULL);
+    gtk_label_set_markup (GTK_LABEL (result), "<span face=\"Courier New\" font=\"15\" color=\"#006633\"bgcolor=\"#00CC66\"><b>The desired quantity:</b></span>");
+    gtk_layout_put(GTK_LAYOUT(layout), result, 10, 350);
     gtk_grid_attach(GTK_GRID(grid), result, 0, 3, 1, 1);
-	number1 = gtk_entry_new();
-	gtk_grid_attach(GTK_GRID(grid), number1, 2, 3, 1, 1);
 
 
-    result = gtk_label_new("  มาตรส่วนที่ต้องการ:  ");
+    number1 = gtk_entry_new();
+    gtk_layout_put(GTK_LAYOUT(layout), number1, 320, 350);
+    gtk_grid_attach(GTK_GRID(grid), number1, 2, 3, 1, 1);
+
+
+    result = gtk_label_new(NULL);
+    gtk_label_set_markup (GTK_LABEL (result), "<span face=\"Courier New\" font=\"15\" color=\"#006633\"bgcolor=\"#00CC66\"><b>The scale required:</b></span>");
+    gtk_layout_put(GTK_LAYOUT(layout), result, 500, 350);
     gtk_grid_attach(GTK_GRID(grid), result, 3, 3, 1, 1);
 
 
-        combo_box2 = gtk_combo_box_text_new();
+
+    combo_box2 = gtk_combo_box_text_new();
     gtk_layout_put(GTK_LAYOUT(layout), combo_box2, 790, 350);
     gtk_grid_attach(GTK_GRID(grid), combo_box2, 4, 3, 1, 1);
 
@@ -1247,22 +1296,42 @@ static void section(GtkApplication *app,GtkWidget *widget, gpointer data) {
     gtk_combo_box_set_active(GTK_COMBO_BOX(combo_box2), 0);
 
 
-    result = gtk_label_new("  ");
-    gtk_grid_attach(GTK_GRID(grid), result, 0, 4, 5, 1);
+    button2 = gtk_button_new();
+    image2 = gtk_image_new_from_file("ปุ่ม2.png");
+    gtk_button_set_image(GTK_BUTTON(button2), image2);
+    gtk_button_set_relief(GTK_BUTTON(button2), GTK_RELIEF_NONE);
+    g_signal_connect(button2, "clicked", G_CALLBACK (do_calculate), (gpointer) window);
+    gtk_layout_put(GTK_LAYOUT(layout), button2, 400, 450);
+    gtk_grid_attach(GTK_GRID(grid), button2, 0, 4, 5, 1);
 
-    gtk_widget_set_size_request (window, 500, 500);
+    result = gtk_label_new(NULL);
+    gtk_label_set_markup (GTK_LABEL (result), "<span font=\"10\" color=\"#006633\"bgcolor=\"#00CC66\"><b>** ปริมาณรับเฉพาะ จำนวนเต็มและทศนิยมเท่านั้น! ตย. 3/4 ถ้วยตวง = 0.75 ถ้วยตวง**</b></span>");
+    gtk_layout_put(GTK_LAYOUT(layout), result, 150, 400);
+    gtk_grid_attach(GTK_GRID(grid), result, 0, 5, 6, 1);
+
+
+    result = gtk_label_new(NULL);
+    gtk_label_set_markup (GTK_LABEL (result), "<span font=\"20\" color=\"#006633\"bgcolor=\"#00CC66\"><b>ผลลัทธ์</b></span>");
+    gtk_layout_put(GTK_LAYOUT(layout), result, 380, 600);
+    gtk_grid_attach(GTK_GRID(grid), result, 0, 7, 7, 1);
+
+    output = gtk_label_new(NULL);
+    gtk_label_set_markup (GTK_LABEL(output), "<span font=\"30\" color=\"#006633\"><b> </b></span>");
+    gtk_layout_put(GTK_LAYOUT(layout), output, 400, 700);
+    gtk_grid_attach(GTK_GRID(grid), output, 0, 8, 8, 1);
+
     box = gtk_box_new (TRUE, 1);
     gtk_box_pack_end (GTK_BOX (box), button, TRUE, TRUE, 1);
 
-    gtk_widget_show_all (window);
+    gtk_widget_show_all(window);
 
 }
-static void ice_jung(GtkWidget *widget, gpointer data) {
-    char buffer[32];
-    snprintf(buffer, sizeof(buffer), "iceice2");
+// static void ice_jung(GtkWidget *widget, gpointer data) {
+//     char buffer[32];
+//     snprintf(buffer, sizeof(buffer), "iceice2");
 
-    gtk_label_set_text(GTK_LABEL(result), buffer);
-}
+//     gtk_label_set_text(GTK_LABEL(result), buffer);
+// }
 
 static void activate (GtkApplication *app,gpointer user_data)
 {
@@ -1270,52 +1339,50 @@ static void activate (GtkApplication *app,gpointer user_data)
   GtkWidget *box;
   GtkWidget *grid;
   GtkWidget *button;
-  GtkWidget *icebutton;
+  // GtkWidget *icebutton;
   GtkWidget *layout;
-  GtkWidget *image,*label;
+  GtkWidget *image;
 
 
   window = gtk_application_window_new (app);
   gtk_window_set_title (GTK_WINDOW (window), "cp61");
   gtk_container_set_border_width (GTK_CONTAINER (window), 0);
-  gtk_window_set_default_size(GTK_WINDOW(window), 500, 500);
+  gtk_window_set_default_size(GTK_WINDOW(window), 600, 600);
    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
   layout = gtk_layout_new(NULL, NULL);
     gtk_container_add(GTK_CONTAINER (window), layout);
     gtk_widget_show(layout);
-	label = gtk_label_new(NULL);
     image = gtk_image_new_from_file("bg_star.jpg");
     gtk_layout_put(GTK_LAYOUT(layout), image, 0, 0);
 
-  gtk_label_set_markup (GTK_LABEL (label), "<span face=\"Courier New\" font=\"30\" color=\"#FF6699\"  bgcolor=\"#FFE4A4\"><b>The changer</b></span>");
-  gtk_layout_put(GTK_LAYOUT(layout), label, 110, 10);
+    image = gtk_image_new_from_file("logo.png");
+  gtk_layout_put(GTK_LAYOUT(layout), image, 100, 10);
   grid = gtk_grid_new ();
   gtk_container_add (GTK_CONTAINER (window), grid);
 
   button = gtk_button_new();
   image = gtk_image_new_from_file("1.png");
   gtk_button_set_image(GTK_BUTTON(button), image);
-	gtk_button_set_relief(GTK_BUTTON(button), GTK_RELIEF_NONE);
-	gtk_button_set_relief(GTK_BUTTON(button), GTK_RELIEF_NONE);
+    gtk_button_set_relief(GTK_BUTTON(button), GTK_RELIEF_NONE);
   g_signal_connect (button, "clicked", G_CALLBACK (section), (gpointer) window);
-  gtk_layout_put(GTK_LAYOUT(layout), button, 150, 100);
+  gtk_layout_put(GTK_LAYOUT(layout), button, 200, 250);
 
   gtk_grid_attach (GTK_GRID (grid), button, 250, 0, 1, 1);
 
 
-  icebutton = gtk_button_new_with_label (" Button 2");
-  g_signal_connect (icebutton, "clicked", G_CALLBACK (ice_jung), NULL);
-  gtk_layout_put(GTK_LAYOUT(layout), icebutton, 200, 250);
-  gtk_grid_attach (GTK_GRID (grid), icebutton, 250, 2, 2, 1);
+  // icebutton = gtk_button_new_with_label (" Button 2");
+  // g_signal_connect (icebutton, "clicked", G_CALLBACK (ice_jung), NULL);
+  // gtk_layout_put(GTK_LAYOUT(layout), icebutton, 200, 250);
+  // gtk_grid_attach (GTK_GRID (grid), icebutton, 250, 2, 2, 1);
 
 
   button = gtk_button_new();
   image = gtk_image_new_from_file("5.png");
-	gtk_button_set_image(GTK_BUTTON(button), image);
-	gtk_button_set_relief(GTK_BUTTON(button), GTK_RELIEF_NONE);
-	gtk_button_set_relief(GTK_BUTTON(button), GTK_RELIEF_NONE);
+    gtk_button_set_image(GTK_BUTTON(button), image);
+    gtk_button_set_relief(GTK_BUTTON(button), GTK_RELIEF_NONE);
+    gtk_button_set_relief(GTK_BUTTON(button), GTK_RELIEF_NONE);
   g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_widget_destroy), window);
-  gtk_layout_put(GTK_LAYOUT(layout), button, 350, 400);
+  gtk_layout_put(GTK_LAYOUT(layout), button, 400, 480);
   gtk_grid_attach (GTK_GRID (grid), button, 250, 3, 3, 1);
 
 
