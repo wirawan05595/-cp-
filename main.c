e083a625b80717eeb37109072617def3397e62f0
ppllelp <basementple@gmail.com>
2019-04-17 11:25:02 +0700

    Update main.c

--- main.c
+++ main.c
@@ -257,6 +257,20 @@ if (check == 0){
         }
     }
 
+    else if (strcmp(select_scale1, scale1[1]) == 0 && strcmp(select_scale2, scale2[7]) == 0)
+    {
+        double box = num1*256;
+        if (fmod(box,1) == 0)
+        {
+            snprintf(buffer, sizeof(buffer), "%.0lf %s",box,select_scale1);
+            gtk_label_set_text(GTK_LABEL(output), buffer);
+        }
+        else {
+            snprintf(buffer, sizeof(buffer), "%.02lf %s",box,select_scale1);
+            gtk_label_set_text(GTK_LABEL(output), buffer);
+        }
+    }
+
 }
 }
 static void libraryscale(GtkApplication *app,GtkWidget *widget, gpointer data) {
