077d05d2a001a7e70aad3523656ace75104c3f34
wirawan05595 <61070209@ktmil.ac.th>
2019-04-17 10:41:18 +0700

    Update main.c

--- main.c
+++ main.c
@@ -116,6 +116,20 @@ if (check == 0){
         }
     }
 
+    else if (strcmp(select_scale1, scale1[0]) == 0 && strcmp(select_scale2, scale2[6]) == 0)
+    {
+        double box = num1/0.00520833333;
+        if (fmod(box,1) == 0)
+        {
+            snprintf(buffer, sizeof(buffer), "%.0lf %s",box,select_scale1);
+            gtk_label_set_text(GTK_LABEL(output), buffer);
+        }
+        else {
+            snprintf(buffer, sizeof(buffer), "%.02lf %s",box,select_scale1);
+            gtk_label_set_text(GTK_LABEL(output), buffer);
+        }
+    }
+
 
 }
 }
